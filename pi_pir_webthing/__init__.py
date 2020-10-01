69c6b1b8aed391898d6aeaed1cdcd254036bc8a2
grro <gregor.roth@1und1.de>
2020-10-01 13:58:23 +0200

    loglevel set to info

--- pi_pir_webthing/__init__.py
+++ pi_pir_webthing/__init__.py
@@ -31,8 +31,7 @@ def main():
 
 
 if __name__ == '__main__':
-    log_level = os.environ.get("LOGLEVEL", "INFO")
-    logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.getLevelName(log_level), datefmt='%Y-%m-%d %H:%M:%S')
+    logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 
     main()
 
